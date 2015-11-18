a8aca4fb1fa1c71dc931bed28778529d883fb961
michaell <superinsky@gmail.com>
2015-11-18 17:46:26 +1100

    change to server.on way to create server

--- readfile_server.js
+++ readfile_server.js
@@ -2,13 +2,22 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(request, response) {
-  response.writeHead(200);
+var server = http.createServer();
 
+server.on('request',function(request, response){
+  response.writeHead(200);
   fs.readFile('index.html', function(error, content){
     response.write(content);
     response.end();
     //response.end(content); you can remove response.write by do it this way
-  });
+});
 
-}).listen(8080);
+server.on('request', function(request, response) {
+  console.log("New request coming in...");
+});
+
+server.on('close', function() {
+  console.log("Closing down the server...");
+});
+
+server.listen(8080);
