a1bfc78b90324a2a1c1d86392a9be17291ba05c6
michaell <superinsky@gmail.com>
2015-11-20 17:02:12 +1100

    add stream example 2

--- streams_simple.js
+++ streams_simple.js
@@ -4,3 +4,9 @@ http.createServer(function(request,response){
   response.writeHead(200);
   request.pipe(response);
 }).listen(8080)
+
+
+//example 2
+var fs = require('fs');
+var file = fs.createReadStream('fruits.txt');
+file.pipe(process.stdout);
